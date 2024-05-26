60d02bf0b57f20ac96d6f7c4eded47c5a9de03d7
Portgas·D·Asce <lupengkunmc@163.com>
2024-05-26 18:16:27 +0800

    poll ok

--- src/multiplex/poll.h
+++ src/multiplex/poll.h
@@ -7,9 +7,11 @@
 
 class Poll {
 public:
-    const static int MX = 10240;
+    const static int MX = 2048;
 private:
     struct pollfd _fds[Poll::MX];
+    int _mx;
+    bool _flag;
     std::function<void(int)> _read_callback;
     std::function<void(int)> _write_callback;
     std::string _name;
@@ -19,47 +21,56 @@ public:
                   std::string name)
         : _read_callback(read_callback),
         _write_callback(write_callback), _name(name) {
+        _mx = 0;
+        _flag = 0;
         for(int i = 0; i < MX; ++i) {
             _fds[i].fd = -1;
         }
     }
 
     void add(int fd) {
-        _fds[fd].fd = fd;
-        _fds[fd].events = POLLRDNORM | POLLWRNORM;
-        printf("pay attention: %d\n", fd);
+        _fds[_mx].fd = fd;
+        _fds[_mx].events = POLLRDNORM | POLLWRNORM;
+        _mx++;
+        printf("%s add attention: %d\n", _name.c_str(), fd);
+    }
+
+    void mod(int fd) {
+        // do nothing
+        //printf("%s mod attention: %d\n", _name.c_str(), fd);
     }
 
     void rm(int fd) {
-        _fds[fd].fd = -1;
-        printf("remove attention: %d\n", fd);
+        _flag = true;
+        printf("%s remove attention: %d\n", _name.c_str(), fd);
     }
 
     void dispatch() {
         while(true) {
-            struct pollfd temp[Poll::MX];
-            for(int i = 0; i < MX; ++i) {
-                temp[i] = _fds[i];
-            }
-            int cnt = poll(temp, MX, 0);
+            int cnt = poll(_fds, _mx, 0);
             if(cnt < 0) {
                 perror("poll error");
                 exit(1);
             }
 
-            //printf("%s: %d\n", _name.c_str(), cnt);
-
-            for(int i = 0; cnt > 0 && i < MX; ++i) {
-                if(temp[i].revents & POLLRDNORM) {
-                    cnt--;
+            for(int i = 0; cnt > 0; ++i) {
+                _flag = false;
+                int temp = 0;
+                if(_fds[i].revents & POLLRDNORM) {
+                    temp = 1;
                     _read_callback(_fds[i].fd);
                 }
 
-                if(temp[i].revents & POLLWRNORM) {
-                    cnt--;
+                if(_fds[i].revents & POLLWRNORM) {
+                    temp = 1;
                     _write_callback(_fds[i].fd);
                 }
-                //cnt -= flag;
+                cnt -= temp;
+                if(_flag) {
+                    _mx--;
+                    _fds[i] = _fds[_mx];
+                    --i;
+                }
             }
         }
     }
