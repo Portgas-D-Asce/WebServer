4c383a5ca7c949dc884f1225a05543144d582e82
Portgas·D·Asce <lupengkunmc@163.com>
2024-06-21 10:57:50 +0800

    replace assign with copy when writing to ring

--- src/buffer/ring_buffer.h
+++ src/buffer/ring_buffer.h
@@ -1,10 +1,9 @@
 #ifndef WEBSERVER_RING_BUFFER_H
 #define WEBSERVER_RING_BUFFER_H
 #include <string>
-#include <mutex>
 #include <vector>
+#include <mutex>
 #include <memory>
-#include <unistd.h>
 #include "../common/socket.h"
 
 class RingBuffer {
@@ -12,12 +11,11 @@ private:
     std::string _buff;
     // _start 起点
     // _end 终点（不包含）
-    // _center 检查消息分隔符检查到哪里了
-    size_t _start, _center, _end;
+    size_t _start, _end;
     mutable std::mutex _mtx;
 public:
     // n 要求必须为 2 的幂
-    explicit RingBuffer(size_t n) : _buff(n, '\0'), _start(0), _center(0), _end(0) {}
+    explicit RingBuffer(size_t n) : _buff(n, '\0'), _start(0), _end(0) {}
 
     // _start == _end 表示没有数据
     bool empty() const {
@@ -30,25 +28,32 @@ public:
         return _end - _start;
     }
 
+    size_t capacity() const {
+        return _buff.size();
+    }
+
+    size_t clear() {
+        _start = _end;
+    }
+
     // 向缓冲区中写入数据
-    void write_msg(const char *s, size_t len) {
+    void write_msg(const char* msg, size_t m, bool flag = true) {
         // 总数据 = 当前数据 + 需要写入数据, 如果大于等于缓冲区大小, 则扩容, 直到可以容纳数据
         // 等于时也要扩容, 因为环形缓冲区不能放满
-        std::lock_guard<std::mutex> lg(_mtx);
-        size_t multi = 0;
-        while((size() + len) >= (_buff.size() << multi)) ++multi;
+        size_t multi = 0, total = size() + m;
+        while(total >= (_buff.size() << multi)) ++multi;
+        if(flag) std::lock_guard<std::mutex> lg(_mtx);
         if(multi) _expand(multi);
 
         // 将数据写入缓冲区
-        size_t n = _buff.size(), mask = n - 1;
-        for(size_t i = 0; i < len; ++i, ++_end) {
-            _buff[_end & mask] = s[i];
-        }
+        _copy_to_ring(msg, m);
     }
 
     int recv_msg(const std::shared_ptr<Socket>& sock, const std::string& split, std::vector<std::string>& msgs) {
         int total = 0;
-        char buff[4096] = {0};
+        char buff[8192] = {0};
+        // 数据读完之后从 center 处开始查找消息分隔符号
+        size_t center = end();
         while(true) {
             int cnt = sock->sock_recv(buff, sizeof(buff));
             if(cnt == -1) {
@@ -58,17 +63,16 @@ public:
                 break;
             }
             total += cnt;
-            write_msg(buff, cnt);
+            write_msg(buff, cnt, false);
         }
-        msgs = _read_msg(split);
+        msgs = _read_msg(split, center);
         return total;
     }
 
     int send_msg(const std::shared_ptr<Socket>& sock) {
         std::lock_guard<std::mutex> lg(_mtx);
         if(empty()) return 0;
-        size_t n = _buff.size(), mask = n - 1;
-        size_t rel_start = _start & mask, rel_end = _end & mask;
+        size_t n = capacity(), rel_start = real_start(), rel_end = real_end();
         // 如果数据没有跨越缓冲区末尾, 一次性发送就好了
         if(rel_end > rel_start) {
             int cnt = sock->sock_send(_buff.c_str() + rel_start, rel_end - rel_start);
@@ -88,8 +92,37 @@ public:
         _start += cnt;
         return cnt;
     }
+
 private:
-    void _copy(std::string& t) {
+    size_t start() const {
+        return _start;
+    }
+
+    size_t end() const {
+        return _end;
+    }
+
+    size_t real_start() const {
+        return _start & (capacity() - 1);
+    }
+
+    size_t real_end() const {
+        return _end & (capacity() - 1);
+    }
+
+    void _copy_to_ring(const char* msg, size_t m) {
+        size_t n = _buff.size(), rel_end = _end & (n - 1);
+        if(n - rel_end >= m) {
+            std::copy(msg, msg + m, _buff.begin() + rel_end);
+        } else {
+            size_t temp = n - rel_end;
+            std::copy(msg, msg + temp, _buff.begin() + rel_end);
+            std::copy(msg + temp, msg + m, _buff.begin());
+        }
+        _end += m;
+    }
+
+    void _copy_from_ring(std::string& t) {
         size_t mask = _buff.size() - 1, rel_start = _start & mask, rel_end = _end & mask;
         if(rel_start <= rel_end) {
             std::copy(_buff.begin() + rel_start, _buff.begin() + rel_end, t.begin());
@@ -103,7 +136,7 @@ private:
         // 申请新缓冲区
         std::string temp(_buff.size() << multi, '\0');
         // 拷贝数据
-        _copy(temp);
+        _copy_from_ring(temp);
         // 更新起点, 终点
         _end -= _start;
         _start = 0;
@@ -112,10 +145,10 @@ private:
     }
 
     // 从缓冲区中读取消息
-    std::vector<std::string> _read_msg(const std::string& split) {
+    std::vector<std::string> _read_msg(const std::string& split, size_t center) {
         if(empty()) return {};
 
-        size_t m = split.size(), mask = _buff.size() - 1;
+        size_t m = split.size(), mask = capacity() - 1;
         // 计算消息分隔符 "hash" 值
         unsigned int tar = 0;
         for(size_t i = 0; i < m; ++i) {
@@ -123,21 +156,22 @@ private:
         }
 
         // 至少要从 start + m - 1 处开始比较
-        _center = std::max(_center, _start + m - 1);
+        center = std::max(center, _start + m - 1);
         unsigned int cur = 0;
-        for(size_t i = _center + 1 - m; i < _center; ++i) {
+        for(size_t i = center + 1 - m; i < center; ++i) {
             cur = (cur << 8) | _buff[i & mask];
         }
 
         // 读取消息
         std::vector<std::string> msgs;
-        for( ;_center < _end; ++_center) {
-            cur = (cur << 8) | _buff[_center & mask];
+        msgs.reserve(10);
+        for( ;center < _end; ++center) {
+            cur = (cur << 8) | _buff[center & mask];
             if(cur != tar) continue;
-            msgs.push_back(std::string(_center - _start + 1, '\0'));
-            _copy(msgs.back());
-            _start = _center + 1;
-            _center = _start + m - 1;
+            msgs.push_back(std::string(center - _start + 1, '\0'));
+            _copy_from_ring(msgs.back());
+            _start = center + 1;
+            center = _start + m - 1;
         }
         return msgs;
     }
